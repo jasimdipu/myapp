5ac674fdc6f4441660a4a6c4fbc33130a291ede4
jasimdipu <dipu.j247@gmail.com>
2021-01-20 20:49:52 +0600

    migration completed

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     # title, slug, author, updated_on, content, created_on, status
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE(), related_name="blog_post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
     updated_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
