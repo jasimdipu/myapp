5c520605d0fdf8f253248c18a809ff3f43a12fb9
jasimdipu <dipu.j247@gmail.com>
2021-01-22 22:08:13 +0600

    comment working but post details not working

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+
+
 # Create your views here.
 
 class PostList(generic.ListView):
-
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 2
 
-class PostDetail(generic.DetailView):
 
+def post_detail(request, slug):
+    template_name = 'PostDetails.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, template_name, {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form
+    })
+
+
+class PostDetail(generic.DetailView):
     model = Post
     template_name = 'PostDetails.html'
