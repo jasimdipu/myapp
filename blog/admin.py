94aa5ba51bc796f1f81fcad4716ba18a797df7d1
jasimdipu <dipu.j247@gmail.com>
2021-01-22 21:12:58 +0600

    comments model is created

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 # Register your models here.
@@ -9,6 +9,17 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("status", 'title')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ("active", 'created_on')
+    search_fields = ['name', 'email']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
 
 
 admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ class PostList(generic.ListView):
 
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 2
 
 class PostDetail(generic.DetailView):
 
