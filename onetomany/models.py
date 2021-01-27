7d4eae338f26662d13702f4b61b9ccc3554a61cf
jasimdipu <dipu.j247@gmail.com>
2021-01-27 21:42:08 +0600

    one to many relationship

--- onetomany/models.py
+++ onetomany/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "Name {}{}, email {}".format(self.first_name, self.last_name, self.email)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Article: {}, Reporter: {}".format(self.title, self.reporter.first_name)
